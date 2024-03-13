f0a9d21a8dbfd67d77db73e1b32a80d79227b568
Dustin Schimel <dustin.schimel@gmail.com>
2024-03-13 01:02:16 -0600

    Fixed prefabs

--- Assets/Scripts/BuildingSystem.cs
+++ Assets/Scripts/BuildingSystem.cs
@@ -1,18 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class BuildingSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static BuildingSystem current;
+
+    public GridLayout gridLayout;
+    private Grid grid;
+    [SerializeField] private Tilemap mainTilemap;
+    [SerializeField] private TileBase redTile;
+
+    public GameObject straight;
+    public GameObject intersection;
+    public GameObject turn;
+    public GameObject house;
+    public GameObject work;
+
+    private PlaceableObject objectToPlace;
+
+
+    private void Awake()
     {
-        
+        current = this;
+        grid = gridLayout.gameObject.GetComponent<Grid>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            InitializeWithObject(straight);
+        }
+        else if(Input.GetKeyDown(KeyCode.W))
+        {
+            InitializeWithObject(intersection);
+        }
+        else if(Input.GetKeyDown(KeyCode.E))
+        {
+            InitializeWithObject(turn);
+        }
+        else if(Input.GetKeyDown(KeyCode.R))
+        {
+            InitializeWithObject(house);
+        }
+        else if(Input.GetKeyDown(KeyCode.T))
+        {
+            InitializeWithObject(work);
+        }
+    }
+
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit raycastHit))
+        {
+            return raycastHit.point;
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+
+    public Vector3 SnapCoordinateToGrid(Vector3 position)
+    {
+        Vector3Int cellPos = gridLayout.WorldToCell(position);
+        position = grid.GetCellCenterWorld(cellPos);
         
+        return position;
+    }
+
+    public void InitializeWithObject(GameObject prefab)
+    {
+        Vector3 position = SnapCoordinateToGrid(Vector3.zero);
+
+        GameObject obj = Instantiate(prefab, position, Quaternion.identity);
+        objectToPlace = obj.GetComponent<PlaceableObject>();
+        obj.AddComponent<ObjectDrag>();
     }
 }
